41e85735d2a702ae263c811c2a662ee9025fc997
Dawid <dawid.marchewczyk@gmail.com>
2024-05-05 22:55:54 +0200

    show single product feature

--- front-end/src/contants/api.js
+++
@@ -1,7 +0,0 @@
-export const PATH_TO_ENDPOINT_MAPPING = {
-  kobieta: "women",
-  mezczyzna: "men",
-  dziecko: "children",
-};
-
-export const BACKEND_URL = "http://localhost:3000";
--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -8,42 +8,49 @@ import { Favourites } from "./views/Favourites/Favourites";
 import { Layout } from "./components/Layout/Layout";
 import { MainPage } from "./views/MainPage/MainPage";
 import { ProductsList } from "./views/ProductsList/ProductsList";
+import { ProductDetails } from "./views/ProductDetails/ProductDetails";
 import { mainPageLoader } from "./api/mainPageLoader";
 import { productListLoader } from "./api/productListLoader";
+import { productLoader } from "./api/productLoader";
 
 const router = createBrowserRouter([
-    {
-        path: "",
-        element: <Layout />,
-        children: [
-            {
-                path: "/koszyk",
-                element: <Cart />,
-            },
-            {
-                path: "/ulubione",
-                element: <Favourites />,
-            },
-            {
-                path: "/:gender?",
-                element: <MainPage />,
-                loader: mainPageLoader,
-            },
-            {
-                path: "/:gender/:category/:subcategory?",
-                element: <ProductsList />,
-                loader: productListLoader,
-            },
-        ],
-    },
-    {
-        path: "test",
-        element: <Layout />,
-    },
+  {
+    path: "",
+    element: <Layout />,
+    children: [
+      {
+        path: "/koszyk",
+        element: <Cart />,
+      },
+      {
+        path: "/ulubione",
+        element: <Favourites />,
+      },
+      {
+        path: "/:gender?",
+        element: <MainPage />,
+        loader: mainPageLoader,
+      },
+      {
+        path: "/:gender/:category/:subcategory?",
+        element: <ProductsList />,
+        loader: productListLoader,
+      },
+      {
+        path: "/:gender/:category/:subcategory/:productId",
+        element: <ProductDetails />,
+        loader: productLoader,
+      },
+    ],
+  },
+  {
+    path: "test",
+    element: <Layout />,
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <RouterProvider router={router}></RouterProvider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>
 );
